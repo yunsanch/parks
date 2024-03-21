eeac2b5f006bf06435ead0be3333610a5c189ad1
yunsanch <60577975+yunsanch@users.noreply.github.com>
2024-03-20 22:39:45 -0400

    Initial Commit

---
+++ Parks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Parks
+//
+//  Created by Yunior Sanchez on 3/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Parks/ParksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParksApp.swift
+//  Parks
+//
+//  Created by Yunior Sanchez on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct ParksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
